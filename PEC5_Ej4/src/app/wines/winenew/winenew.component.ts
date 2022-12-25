f863f11fc3006b1bfdc51d75830cda81e6192425
Lluis Cortes Frau <lluiscortes@uoc.edu>
2022-12-25 20:41:42 +0100

    Exercise 4

---
+++ PEC5_Ej4/src/app/wines/winenew/winenew.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-winenew',
+  templateUrl: './winenew.component.html',
+  styleUrls: ['./winenew.component.css']
+})
+export class WinenewComponent {
+
+}
