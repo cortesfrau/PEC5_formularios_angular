10d7bcde55b14425725f320bbb2b059738e41aab
Lluis Cortes Frau <lluiscortes@uoc.edu>
2022-12-25 21:51:19 +0100

    Exercise 4

--- PEC5_Ej4/src/app/wines/winenew/winenew.component.ts
+++ PEC5_Ej4/src/app/wines/winenew/winenew.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Wine } from 'src/app/models/wine';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { wineNameValidator } from 'src/app/validators/winename-validator';
 
 @Component({
   selector: 'app-winenew',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./winenew.component.css']
 })
 export class WinenewComponent {
+  public message = "";
+  public wineForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.wineForm = this.fb.group({
+      name: ["", [Validators.required, wineNameValidator()]],
+      price: [0, [Validators.required, Validators.min(1)]],
+      imageUrl: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("^http(s?)://[a-zA-Z0-9-.]+.[a-zA-Z]{2,3}(/S*)?$")
+        ]
+      ],
+      isOnSale: false
+    });
+  }
+
+  createWine() {
+    if (this.wineForm.invalid) {
+      this.message = "Si us plau, corregeix les errades abans d'enviar el formulari.";
+    } else {
+      const wine: Wine = this.wineForm.value;
+      console.log("S'ha creat un nou vi!", wine);
+    }
+  }
 }
